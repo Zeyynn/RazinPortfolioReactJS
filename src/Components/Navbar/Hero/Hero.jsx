4d203ee4f21dc31b8ccccfa5fbe8b5b1e644d758
Zeyynn <134064848+Zeyynn@users.noreply.github.com>
2025-02-13 18:35:34 +0800

    text

--- src/Components/Navbar/Hero/Hero.jsx
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div id='home' className="hero">                      {/*Profile*/}
         <img src={profile_img} className="profile-image" alt =""/>
         <h1><span>I'm Hariz Razin,</span> a student in Malaysia</h1>
-        <p>Currently pursuing in Bachelor of Software Engineering in Universiti Malaysia Pahang Sultan Abdullah and interning at Trigon Tech Sdn Bhd</p>
+        <p>A final year student currently pursuing in Bachelor of Software Engineering in Universiti Malaysia Pahang Sultan Abdullah and interning at Trigon Tech Sdn Bhd</p>
         <div className="hero-action">                         {/*Profile Links to Github and Linkedin*/}
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
             <a href="https://razin-portfolio-react-js.vercel.app/Hariz_Razin_Resume.pdf" download="Hariz_Razin_Resume.pdf" className="hero-resume">My Resume</a>
